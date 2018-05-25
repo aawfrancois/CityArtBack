a4e66312eadecf8dbe8669c2e131f19705235141
aawfrancois <aawfrancois@gmail.com>
2018-05-25 16:50:26 +0200

    mise en place passport js erreur

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,6 @@ api.get('/', (req, res) => {
 
 api.use('/users', passport.authenticate('jwt', { session: false }), user)
 api.use('/tags', tag)
-app.use ('/auth', auth)
+api.use ('/auth', auth)
 
 export default api
