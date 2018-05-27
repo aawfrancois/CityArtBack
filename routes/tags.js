35a750f8fd2dee753661c4f104285c814440aced
aawfrancois <aawfrancois@gmail.com>
2018-05-27 15:26:13 +0200

    add user for tags

--- routes/tags.js
+++ routes/tags.js
@@ -6,7 +6,7 @@ let api = Router()
 
 api.get('/', async (req, res) => {
     let tags = await Tags.findAll()
-    let user = await Users.findOne({ where: { id: Tags.User_id} })
+    let user = await Users.findById(Tags.User_id)
     res.json({ tags, user })
 })
 
